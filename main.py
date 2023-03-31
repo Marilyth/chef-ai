7905110eb849d5dfcb8d1c7b96f4240ddde7b5d3
May Bär <baermay98@gmail.com>
2023-03-31 14:26:01 +0200

    Handle train/generation through parameters

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import Models.Instructions.Transformer
 import os
+import sys
 os.add_dll_directory("C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.2/bin")
 
 
 if __name__ == "__main__":
     transformer = Models.Instructions.Transformer.TransformerTrainer(200, 3, 500, 400, 4, 0.0)
-    #transformer.load_model()
-    #transformer._prepare()
-    #print(transformer.test(transformer.test_set))
-    transformer.train(max_epochs=1)
-    transformer.save_model()
+    if "test" in sys.argv:
+        transformer.load_model()
+        print("Model is ready. Press Enter for a new sample.")
+        while True:
+            ingredients = input("If you want, you can provide a comma seperated list of ingredients now: ")
 
-    print("Model is ready. Press Enter for a new sample.")
-    while True:
-        ingredients = input("If you want, you can provide a comma seperated list of ingredients now: ")
-
-        transformer.generate_recipe(ingredients)
+            transformer.generate_recipe(ingredients)
+    else:
+        transformer.train(max_epochs=4)
+        transformer.save_model()
